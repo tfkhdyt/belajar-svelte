03ca08f6a237f209fcadc5c1cd20709cbc4f4af4
tfkhdyt <tfkhdyt@proton.me>
2022-12-15 08:46:13 +0700

    feat: learn about reactivity

---
+++ svelteKit-app/src/routes/reactivity/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	let quantity = 0;
+	let inventory = [1, 2, 3];
+  let user = { name: 'Taufik' };
+  $: remaining = 10 - quantity;
+  let price = 5;
+  $: totalCost = quantity * price;
+
+	const addToCart = () => {
+    inventory = [...inventory, quantity];
+		quantity++;
+    user.name = 'Hidayat';
+	};
+
+  const increasePrice = () => {
+    price += 2;
+  }
+</script>
+
+<h1>Your shopping cart has {quantity} items.</h1>
+<button on:click={addToCart}> Add to Cart </button>
+
+<h2>{inventory}</h2>
+<h2>{user.name}</h2>
+<div>Remaining inventory: {remaining}</div>
+<div>Item price: {price}</div>
+<button on:click={increasePrice}>Increase price</button>
+<div>Total cost: {totalCost}</div>
