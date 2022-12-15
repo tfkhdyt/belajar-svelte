bec04fbdf2bee60b46d79162a8a8c3e5076d3902
tfkhdyt <tfkhdyt@proton.me>
2022-12-15 14:07:20 +0700

    feat(sandbox): apply the GridTile component

--- sandbox/src/lib/GridTile.svelte
+++ sandbox/src/lib/GridTile.svelte
@@ -1,3 +1,24 @@
-<div>
-  This is GridTile component.
+<script lang="ts">
+	import type { Product } from '../types/product';
+
+	export let product: Product = {
+		title: 'No product provided',
+		src: 'https://img.icons8.com/ios/500/no-image.png',
+		price: '$0'
+	};
+	console.log(product);
+</script>
+
+<div class="h-[50vh] w-full overflow-hidden bg-indigo-600">
+	<div class="relative flex h-full w-full items-center justify-center">
+		<img
+			src={product.src}
+			class="w-full flex-none transition duration-300 ease-in-out md:w-1/2 lg:w-full"
+			alt=""
+		/>
+		<div class="absolute top-0 left-0 text-white">
+			<div class="bg-black p-3 text-2xl font-medium">{product.title}</div>
+			<div class="w-fit bg-black p-3 text-sm">{product.price}</div>
+		</div>
+	</div>
 </div>
--- sandbox/src/routes/+page.svelte
+++ sandbox/src/routes/+page.svelte
@@ -1,5 +1,28 @@
 <script lang="ts">
-	// import GridTile from '$lib/GridTile.svelte';
+	import GridTile from '$lib/GridTile.svelte';
+	import type { Product } from '../types/product';
+
+	let products: Product[] = [
+		{
+			title: 'Mug',
+			price: '$10',
+			src: 'https://cdn.shopify.com/s/files/1/0434/0285/4564/products/Cup-front-black.png?v=1623159405'
+		},
+		{
+			title: 'Shirt',
+			price: '$10',
+			src: 'https://cdn.shopify.com/s/files/1/0434/0285/4564/products/short-sleeve-t-shirt-0.png?v=1622902418'
+		}
+	];
+
+	function updateCost() {
+		products[0].price = '$50';
+	}
 </script>
 
-<!-- <GridTile /> -->
+<main>
+	<button on:click={updateCost} class="my-4 bg-pink-500 p-2 text-white">Change Price</button>
+	{#each products as product (product.title)}
+		<GridTile {product} />
+	{/each}
+</main>
