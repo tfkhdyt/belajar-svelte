d4100084666ca4374ccd97439b045eba9c96c871
tfkhdyt <tfkhdyt@proton.me>
2022-12-15 09:11:29 +0700

    feat: learn about directives

---
+++ svelteKit-app/src/routes/directives/+page.svelte
@@ -0,0 +1,14 @@
+<!-- <button on:click={() => handleClick('Ini alert')}>Click me!</button> -->
+<script lang="ts">
+	let searchedValue = '';
+  let checked = false;
+</script>
+
+<h3>
+	Searched: {searchedValue}
+</h3>
+<input bind:value={searchedValue} type="text" id="myText" />
+<h3>
+	Checked: {checked}
+</h3>
+<input bind:checked type="checkbox" />
